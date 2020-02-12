86e0feb09a09643f416d2df844eb344e8df4ae25
orjon <github@orjon.com>
2020-02-12 10:12:22 +0000

    Adding stops section

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('Transport for London API')
     console.log('www.orjon.com')
 
-    // document.getElementById('#id').addEventListener('click', buttonAction)
+    // document.getElementById('#id').addEventListener('click', showStops)
 
   }
 
@@ -14,10 +14,18 @@ window.addEventListener('DOMContentLoaded', () => {
   //   $('#id').text('text')
   //}
 
-  const $lines = $('.lines')
+  const $lines = $('.lines') //
+  const $stops = $('.stops')
   var lines = []
+  var stops = []
+
+
+  // function showStations() {
+  //
+  // }
 
   function getLines() {
+    $lines.empty()
     console.log('Getting Line information')
     $.ajax({
       method: 'GET',
@@ -32,7 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   function displayLines() {
-    $lines.empty()
+    // $lines.empty()
     lines.forEach(line => {
       var lineReason = ''
       if (line.lineStatuses[0].reason) {
@@ -41,12 +49,9 @@ window.addEventListener('DOMContentLoaded', () => {
         console.log(cropIndex)
         lineReason = lineReason.substring(cropIndex)
       }
-
-
-
-
+      // this.addClass(line.name)
       $lines.append(`
-        <div class='card line'>
+        <div class='card line ${line.id}'>
           <div class='details'>
             <div class='row lineColor ${line.name}'></div>
             <div class='row lineName'>
@@ -65,7 +70,41 @@ window.addEventListener('DOMContentLoaded', () => {
     })
   }
 
+  function getStops() {
+    console.log('Getting stop information')
+    $.ajax({
+      method: 'GET',
+      url: 'https://api.tfl.gov.uk/line/victoria/StopPoints'
+
+    })
+      .then(response => {
+        stops = response
+        // stops.sort()
+        console.log('stops:')
+        console.log(stops)
+        displayStops()
+      })
+  }
+
+  function displayStops() {
+    $stops.empty()
+    stops.forEach(stop => {
+
+      $stops.append(`
+        <div class='details'>
+          <div class='row'>
+            <div class='data'>
+              <p>${stop.commonName}</p>
+            </div>
+          </div>
+        </div>
+        `)
+
+    })
+  }
+
   getLines()
+  getStops()
 
 
 
